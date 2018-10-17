22f22a594d444d4e74e87181f2d88ff58e1e9191
Noel Nevarez <noel3740@gmail.com>
2018-10-16 22:59:38 -0500

    Adding hangman image and link.

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,5 @@
+function portfolioImageClick (portfolioItemName) {
+    if (portfolioItemName.toLowerCase() === "hangman") {
+        window.open('https://noel3740.github.io/Word-Guess-Game/', '_blank')
+    }
+}
