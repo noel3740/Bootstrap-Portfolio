e791262323887409edde85f54e6cf7ea5aa80bda
Noel Nevarez <noel3740@gmail.com>
2018-10-18 11:12:43 -0500

    Adding RPG game link to portfolio

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,5 +1,10 @@
 function portfolioImageClick (portfolioItemName) {
-    if (portfolioItemName.toLowerCase() === "hangman") {
-        window.open('https://noel3740.github.io/Word-Guess-Game/', '_blank')
+    portfolioItemName = portfolioItemName.toLowerCase();
+
+    if (portfolioItemName === "hangman") {
+        window.open('https://noel3740.github.io/Word-Guess-Game/', '_blank');
+    }
+    else if (portfolioItemName === "rpggame") {
+        window.open('https://noel3740.github.io/unit-4-game/', '_blank');
     }
 }
